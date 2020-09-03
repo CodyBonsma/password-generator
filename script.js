dada8c4614d6c2aecfd8f4d397f9c3c2050c23b8
Cody Bonsma <codybonsma@gmail.com>
2020-09-02 22:01:52 -0400

    added a var and function for passwordLength

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+var passwordLength = prompt("Please enter a character amount between 8 and 128 for your password")
+  console.log(passwordLength);
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+    alert("Please choose a number between 8 and 128");
+  }
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
